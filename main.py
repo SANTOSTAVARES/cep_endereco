c95dc544754f43a8647a8d87e292237b99e9de16
SANTOSTAVARES <58869758+SANTOSTAVARES@users.noreply.github.com>
2022-02-10 13:07:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+
+def main() :
+    buscar_cep = input("Informe o CEP.\nExemplo: 31200123\n")
+
+    url = f'http://www.viacep.com.br/ws/{buscar_cep}/json/'
+
+    resposta = requests.get(url)
+    print(resposta.status_code)
+
+    if resposta.status_code == 200:
+        
+        resposta_json = resposta.json()
+        print(resposta_json)
+
+        if len(buscar_cep) == 8 and 'erro' not in resposta_json:
+            
+            bairro = resposta_json['bairro']
+            logradouro = resposta_json['logradouro']
+            localidade = resposta_json['localidade']
+            uf = resposta_json['uf']
+
+            print(bairro)
+            print(logradouro)
+            print(localidade)
+            print(uf)
+        
+        else:
+            print('Esse número de CEP não existe.')
+    else:
+        print('CEP informado é inválido. Tente novamente.')
+        exit()
+
+if __name__ == '__main__':
+	main()
